f1ca28fc9f9a699896b1376bdd437ceffd18a8d1
LuizFernandoFS <lffs2000@outlook.com>
2023-03-19 01:58:02 -0300

    Ajustado versão do Spring

---
+++ src/main/java/com/ifpr/gastronomique/GastronomiqueApiApplication.java
@@ -0,0 +1,13 @@
+package com.ifpr.gastronomique;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GastronomiqueApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GastronomiqueApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ifpr/gastronomique/GastronomiqueApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ifpr.gastronomique;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GastronomiqueApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
